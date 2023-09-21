0625398db5e78fecb1c11ed469c0652563360264
Seungmin Yu <kennesis777@gmail.com>
2023-09-21 12:13:42 +0900

    한달을 그리다

--- App.js
+++ App.js
@@ -24,43 +24,23 @@ let 단기 = 4356;
 let 서기 = 2023;
 let 개천 = 5920;
 
-function 시공간을열다() {
-  let 시공간 = [];
-  // 중첩 for 문 고민
+function 한달을세다() {
+  let 한달 = [];
   for(let 하늘 = 0; 하늘 < 소력().기.수; 하늘++) {
 
-    const 공간 = 0;
+    let 한주 = [];
 
-    const 사람의아름다움 = 666;
+    for(let 땅 = 1; 땅 <= 소력().요.수; 땅++) {
 
-    const 땅 = 소력().요.요일.reduce((시간, 지금, 날짜) => {
-      // 지금 = 시간 += 지금;
+      let 하루 = (하늘 * 소력().요.수) + 땅;
 
-      시공간.push(
-        <View style={{
-          flex: 1,
-          borderWidth: StyleSheet.hairlineWidth,
-          borderColor: '#ffffff',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          flexDirection: 'row'
-        }}>
-        {
-          <View style={{ flexDirection: 'row' }}>
-            {/* <Text style={{ color: 'white' }}>{하늘}</Text> */}
-            {/* <Text style={{ color: 'white' }}>{땅}</Text> */}
-            {/* <Text style={{ color: 'transparent' }}>{사람의아름다움}</Text> */}
-            <Text style={{ color: 'white' }}>{날짜}</Text>
-          </View>
-        }
-        </View>
-      );
+      한주.push(하루);
+    }
 
-      return 지금, 하늘;
-    });
+    한달.push(한주);
 
   }
-  return 시공간;
+  return 한달;
 }
 
 function 소력() {
@@ -100,6 +80,84 @@ function 소력() {
   };
 }
 
+function 한달을그리다(한달) {
+  // console.log(한달);
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'column'
+      }}
+    >
+      {한달.map((한주) => 한주를그리다(한주))}
+    </View>    
+  );
+}
+
+function 한주를그리다(한주) {
+  // console.log(한주);
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        {한주.map((하루) => 하루를그리다(하루))}
+    </View>
+  );
+}
+
+function 하루를그리다(하루) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: 'black',
+        height: 100,
+      }}>
+      <Text style={{ color: 'black' }} key={하루}>{하루}</Text>
+    </View>
+  );
+}
+
+function 요일(날짜) {
+  let 요일 = '';
+  switch (날짜) {
+    case 0:
+      요일 = '토';
+      break;
+    case 1:
+      요일 = '일';
+      break;
+    case 2:
+      요일 = '월';
+      break;
+    case 3:
+      요일 = '화';
+      break;
+    case 4:
+      요일 = '수';
+      break;
+    case 5:
+      요일 = '목';
+      break;
+    case 6:
+      요일 = '금';
+      break;
+  
+    default:
+      break;
+  }
+  return 요일;
+}
+
 // 오늘을 어떻게 알 것인가?
 
 function App() {
@@ -124,52 +182,29 @@ function App() {
             flexDirection: 'column',
             backgroundColor: isDarkMode ? 'black' : 'white',
           }}>
-            <Text style={{ alignSelf: 'center' }}>
+            <Text style={{ alignSelf: 'center', color: 'blue' }}>
               {달력}
             </Text>
-            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around' }}>
-              { 소력().요.요일.map((영원한, 순간) => {
-                let 요일 = '';
-                switch (순간) {
-                  case 0:
-                    요일 = '토';
-                    break;
-                  case 1:
-                    요일 = '일';
-                    break;
-                  case 2:
-                    요일 = '월';
-                    break;
-                  case 3:
-                    요일 = '화';
-                    break;
-                  case 4:
-                    요일 = '수';
-                    break;
-                  case 5:
-                    요일 = '목';
-                    break;
-                  case 6:
-                    요일 = '금';
-                    break;
-                
-                  default:
-                    break;
-                }
-                return <Text key={순간}>{요일}</Text>
-              }) }
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                paddingVertical: 10
+              }}>
+              { 소력().요.요일.map((날짜, 순서) => <Text key={순서}>{요일(날짜)}</Text>) }
             </View>
-            <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#000000' }}>
-              { 시공간을열다() }
-            </View>
+
+            { 한달을그리다(한달을세다()) }
+
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  textColor: 'black',
-});
+// const styles = StyleSheet.create({
+//   textColor: 'black',
+// });
 
 export default App;
